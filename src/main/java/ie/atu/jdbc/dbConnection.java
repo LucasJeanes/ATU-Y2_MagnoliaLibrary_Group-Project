d4e1dbaafecc3ec3ab4e0015cd90d0f499c0ca0b
Lucas Jeanes <g00410024>
2023-04-03 18:08:20 +0100

    Added comments for explanation

--- src/main/java/ie/atu/jdbc/dbConnection.java
+++ src/main/java/ie/atu/jdbc/dbConnection.java
@@ -12,14 +12,18 @@ public class dbConnection {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
         log =Logger.getLogger(dbConnection.class.getName());
     }
-
+//  THIS 'properties' function retrieves login details & URL for database connection
     public static void main(String[] args) throws Exception {
         log.info("Loading application properties");
         Properties properties = new Properties();
         properties.load(dbConnection.class.getClassLoader().getResourceAsStream("application.properties"));
 
         log.info("Connecting to the database");
+
+//THIS LINE OF CODE TO CONNECT TO DATABASE!!!!!
         Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
         log.info("Database connection test: " + connection.getCatalog());
 
         log.info("Create database schema");
@@ -28,8 +32,10 @@ public class dbConnection {
         while (scanner.hasNextLine()) {
             statement.execute(scanner.nextLine());
         }
-/*
-      //Class template
+
+//      This is an example from the online guide I linked to insert data into a table that it creates.
+//      LINK TO THE GUIDE >>>>>> https://learn.microsoft.com/en-us/azure/azure-sql/database/connect-query-java?view=azuresql
+        /*
 		Todo todo = new Todo(1L, "configuration", "congratulations, you have set up JDBC correctly!", true);
         insertData(todo, connection);
         todo = readData(connection);
