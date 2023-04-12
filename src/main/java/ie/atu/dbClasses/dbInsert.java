20794a3526c0d408ab1a3687874a23d621eda09c
Lucas Jeanes <g00410024>
2023-04-12 17:07:49 +0100

    Updated properties file to work

--- src/main/java/ie/atu/dbClasses/dbInsert.java
+++ src/main/java/ie/atu/dbClasses/dbInsert.java
@@ -15,11 +15,11 @@ public class dbInsert {
         System.out.println("Connecting to the database");
         Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
 
-        public static String insertTable = "Boob";
-        public static String insertColumn = "Boob2";
-        public static String insertNewValue = "Boob3";
-        public static String refID = "Boob4";
-        public static String refValue = "Boob5";
+        String insertTable = "Boob";
+        String insertColumn = "Boob2";
+        String insertNewValue = "Boob3";
+        String refID = "Boob4";
+        String refValue = "Boob5";
         
         String selectSQL = "INSERT INTO ?" + "VALUES ?";
         
@@ -32,14 +32,14 @@ public class dbInsert {
             insertStatement.setString(3,insertNewValue);
             insertStatement.setString(4,refID);
             insertStatement.setString(5,refValue);
-            stmt.executeUpdate();
+            insertStatement.executeUpdate();
 
-            int inserted = (statement.executeUpdate(insertStatement.toString()));
+            int inserted = (insertStatement.executeUpdate(insertStatement.toString()));
             System.out.println("The following has successfully been inserted: " + inserted);
         } catch (SQLException ex) {
 
             System.out.println("Record insert failed.");
-            e.printStackTrace();
+            ex.printStackTrace();
         }
         // Close the connection
         connection.close();
