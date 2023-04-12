1922b29783bf518eb9609307b417bc421708856f
dewanhasan99 <g00410156@atu.ie>
2023-04-12 20:17:09 +0100

    Update Class Working

--- src/main/java/ie/atu/dbClasses/dbUpdate.java
+++ src/main/java/ie/atu/dbClasses/dbUpdate.java
@@ -1,38 +1,51 @@
 package ie.atu.dbClasses;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 
 import java.sql.SQLException;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import ie.atu.jdbc.TestdbConnection;
 import ie.atu.jdbc.dbConnection;
 
 import java.sql.*;
 import java.sql.Connection;
+import java.util.Properties;
 
+//"UPDATE ? SET ? = ? WHERE ? = ?"
 public class dbUpdate {
-    public static String updateTable = "Boob";
-    public static String updateColumn = "nipple";
-    public static String newValue = "smallerNipple";
-    public static String refID = "Chest";
-    public static String refValue = "legal";
+        public static void main(String[] args) throws IOException, SQLException {
+        String TableName = "Books";
+        String SetColumnName = "name";
+        String NewName = "DewansBookLASTONEPLS";
+        String refColumn = "book_id";
+        int IDNumber = 16;
 
+        System.out.println("Loading application properties");
+        Properties properties = new Properties();
+        properties.load(TestdbConnection.class.getClassLoader().getResourceAsStream("application.properties"));
+        System.out.println("Connecting to the database");
+        Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
 
-    public static void main(String[] args) {
+        try
+        {
+            String updateSQL = "UPDATE Books SET name = ? WHERE " + refColumn + " = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(updateSQL);
 
+            //updateStatement.setString(1,TableName);
 
+            //updateStatement.setString(1,SetColumnName);
+            updateStatement.setString(1,NewName);
+            updateStatement.setInt(2,IDNumber);
 
-        try (Connection connection = dbConnection.getConnection();
-             Statement statement = connection.createStatement()) {
-            PreparedStatement updateStatement = connection.prepareStatement ("UPDATE ? SET ? = ? WHERE ? = ?");
-            updateStatement.setString(1,updateTable);
-            updateStatement.setString(2,updateColumn);
-            updateStatement.setString(3,newValue);
-            updateStatement.setString(4,refID);
-            updateStatement.setString(5,refValue);
-            int rowsUpdated = (statement.executeUpdate(updateStatement.toString()));
+            int rowsUpdated = updateStatement.executeUpdate();
             System.out.println("Rows updated: " + rowsUpdated);
+
+            connection.close();
+            updateStatement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
