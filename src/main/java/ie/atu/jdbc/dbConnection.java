afd7a758e778eebde7ee535930c886543b3ce612
Lucas Jeanes <g00410024>
2023-05-01 16:55:06 +0100

    Fixed properties file for dbConnection

--- src/main/java/ie/atu/jdbc/dbConnection.java
+++ src/main/java/ie/atu/jdbc/dbConnection.java
@@ -5,16 +5,10 @@ import java.util.*;
 
 public class dbConnection {
     public Connection connection() throws Exception {
-        String URL = "jdbc:mysql://localhost:3306/magnolia_rebornlib";
-        String USERNAME = "root";
-        String PASSWORD = "password";
-
-        //Class.forName("com.mysql.cj.jdbc.Driver");
         Properties properties = new Properties();
         properties.load(dbConnection.class.getClassLoader().getResourceAsStream("application.properties"));
         System.out.println("Connecting to the database");
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        //Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+        Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
         System.out.println("Connected to database: " + connection.getCatalog());
         return connection;
     }
